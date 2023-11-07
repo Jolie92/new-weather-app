3f78592b3aa3357d32d6c9fa9174e99d212e050e
Jolie92 <lauraloriga92@gmail.com>
2023-11-07 11:36:09 +0000

    github link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by Laura Loriga and is {""}
+        <a href="https://github.com/Jolie92/new-weather-app"
+        target="_blank"> open-sourced on GitHub </a>
+      </footer>
     </div>
   );
 }
