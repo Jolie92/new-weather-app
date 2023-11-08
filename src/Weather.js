060e9b144a4a223effd6b30ce281ccbdc67082fc
Jolie92 <lauraloriga92@gmail.com>
2023-11-08 11:58:43 +0000

    css changes

--- src/Weather.js
+++ src/Weather.js
@@ -8,7 +8,7 @@ return <div className="Weather">
             <div className="col-9">
                 <input
                 type="search"
-                placeholder="Enter  city..."
+                placeholder="Enter city..."
                 className="form-control"
                 autoFocus="on"/>
                 
@@ -19,14 +19,14 @@ return <div className="Weather">
         </div>
     </form>
     <h1>New York</h1>
-    <ul>
+    <ul className="description">
         <li>Wednesday 7:00</li>
         <li>Mostly Cloudy</li>
     </ul>
     <div className="row mt-3">
         <div className="col-6">
             <div className="clearfix">
-            <img src="" alt="Mostly Cloudy"/>
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII=" alt="Mostly Cloudy"/>
             <span className="temperature">6</span>
             <span className="unit">°C</span>
             
