c0a61f358e597d10522c784bad5757e71ec8dcef
Jolie92 <lauraloriga92@gmail.com>
2023-11-08 10:13:06 +0000

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       Weather App
       <footer>
         This project was coded by Laura Loriga and is {""}
         <a href="https://github.com/Jolie92/new-weather-app"
-        target="_blank"> open-sourced on GitHub </a>
+        target="_blank" rel="noreferrer"> open-sourced on GitHub </a>
       </footer>
+    </div>
     </div>
   );
 }
