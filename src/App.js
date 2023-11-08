35a89d6ab06e0faa6ac6ed98d1617176730bdab3
Jolie92 <lauraloriga92@gmail.com>
2023-11-08 11:28:44 +0000

    add basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
       Weather App
+      <Weather/>
       <footer>
         This project was coded by Laura Loriga and is {""}
         <a href="https://github.com/Jolie92/new-weather-app"
---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+return <div className="Weather">
+    <form>
+        <div className="row">
+            <div className="col-9">
+                <input
+                type="search"
+                placeholder="Enter  city..."
+                className="form-control"
+                autoFocus="on"/>
+                
+            </div>
+            <div className="col-3">
+                <input type="submit" value="Search" className="btn btn-primary w-100"/>
+            </div>
+        </div>
+    </form>
+    <h1>New York</h1>
+    <ul>
+        <li>Wednesday 7:00</li>
+        <li>Mostly Cloudy</li>
+    </ul>
+    <div className="row mt-3">
+        <div className="col-6">
+            <div className="clearfix">
+            <img src="" alt="Mostly Cloudy"/>
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>
+            
+        </div>
+        </div>
+        </div>
+        <div className="col-6">
+            <ul>
+            <li>Humidity: 59%</li> 
+            <li>Wind: 7.72 km/h </li>
+            </ul>
+        </div>
+    </div>
+
+}
