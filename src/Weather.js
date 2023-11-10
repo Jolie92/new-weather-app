a2c87c29c774cad478b2d5627715ef9f92fc9434
Jolie92 <lauraloriga92@gmail.com>
2023-11-10 14:32:00 +0000

    add API

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Weather.css";
+import axios from "axios";
 
-export default function Weather() {
-return <div className="Weather">
+export default function Weather(props) {
+    
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    function handleResponse (response){
+    setWeatherData({
+        ready: true,
+        temperature:response.data.main.temp,
+        humidity: response.data.main.humidity,
+        date:"Wednesday 07:00",
+        description: response.data.weather[0].description,
+        iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+        wind: response.data.wind.speed,
+        city: response.data.name
+    });
+        
+    }
+    if (weatherData.ready){
+return (
+<div className="Weather">
     <form>
         <div className="row">
             <div className="col-9">
@@ -18,33 +36,42 @@ return <div className="Weather">
             </div>
         </div>
     </form>
-    <h1>New York</h1>
+    <h1>{weatherData.city}</h1>
     <ul className="description">
-        <li>Wednesday 7:00</li>
-        <li>Mostly Cloudy</li>
+        <li>{weatherData.date}</li>
+        <li className="text-capitalize">{weatherData.description}</li>
     </ul>
     <div className="row mt-3">
         <div className="col-6">
-            <div className="clearfix">
-            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII=" 
-            alt="Mostly Cloudy"/>
+            <div className="d-flex">
+            <img src={weatherData.iconUrl} 
+            alt={weatherData.description} 
+            className="float-left"/>
             
-            
-            <span className="temperature">6</span>
+            <div className="float-left">
+            <span className="temperature">{Math.round(weatherData.temperature)}</span>
             <span className="unit">°C</span>
             </div>
         </div>
         </div>
-       
         
-        <div className="col-9">
-            <div className="description2">
+        <div className="col-6">
             <ul>
-            <li>Humidity: 59%</li> 
-            <li>Wind: 7.72 km/h </li>
+            <li>Humidity: {weatherData.humidity}% </li> 
+            <li>Wind: {weatherData.wind} km/h </li>
             </ul>
             </div>
              </div>
         </div>
+);
+    }
+else {
+const apiKey = "3980a7c8f2a782241a093131b099f993";
+   
+    let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(handleResponse);
 
+return "Loading...";
 }
+    }
+    
